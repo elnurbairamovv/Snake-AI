0a1aab4c276727361bd2e81e6f784ff03228160d
elnurbairamovv <elnur.bairamov@stu.ocsb.ca>
2026-09-04 13:12:59 -0400

    I tried to fix a bug but I got stuck and ended up not making progress
    I hate this bug

--- snake.py
+++ snake.py
@@ -17,25 +17,28 @@ def main() -> None:
     food_y = randint(0, ROWS) * TILE_SIZE
     food = pygame.Rect(left=food_x, top=food_y, width=TILE_SIZE, height=TILE_SIZE)
     # snake is going to be a list because it is made up of many rectangles
-    snake = []
+    # snake = []
     
     running = True                      # this variable controls whether the game is running or not running
     while running:
         window.fill(BLACK)
-        draw_grid(window=window)
+        # draw_grid(window=window)
+        
         for event in pygame.event.get():        # track every event that the user can do
             if event.type == pygame.QUIT:       # quit the game if the user clicks the X button at the top right
                 running = False
 
-        pygame.draw.rect(surface=window, color="red", rect=food)
+        pygame.draw.rect(surface=window, color=RED, rect=food)
         pygame.display.update()                 # refresh game window
         clock.tick(10)                          # 10 frames per second
 
-def draw_grid(window):
+
+def draw_grid(window: pygame.Surface):
     for x in range(0, GAME_WIDTH, TILE_SIZE):
         for y in range(0, GAME_HEIGHT, TILE_SIZE):
             rect = pygame.Rect(left=x, top=y, width=TILE_SIZE, height=TILE_SIZE)
             pygame.draw.rect(surface=window, color=WHITE, rect=rect, width=1)
+
 
 # make sure that the code is being accessed from the snake.py module
 if __name__ == "__main__":
@@ -47,6 +50,8 @@ if __name__ == "__main__":
     COLUMNS = int(GAME_WIDTH / TILE_SIZE)
     WHITE = (200, 200, 200)
     BLACK = (0, 0, 0)
+    RED = (255, 116, 108)
+    GREEN = (193, 225, 193)
 
     board_array = []
 
