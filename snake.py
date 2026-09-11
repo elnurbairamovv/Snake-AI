8f76aa44574496d02be77c87ed6bd15e1ff1fb38
elnurbairamovv <elnur.bairamov@stu.ocsb.ca>
2026-09-11 13:15:42 -0400

    made progress on the information input. Added bunch of comments for why I added each input.

--- snake.py
+++ snake.py
@@ -81,6 +81,9 @@ class Snake:
             pygame.display.update()                 # refresh game window
             clock.tick(10)                          # 10 frames per second
 
+            print(f"snake x: {self.snake[0].x}")
+            print(f"snake y: {self.snake[0].y}")
+
 
     def get_random_pos(self) -> tuple[float, float]:
         random_x, random_y = (randint(0, Snake.COLUMNS - 1) * Snake.TILE_SIZE, randint(0, Snake.ROWS - 1) * Snake.TILE_SIZE)
@@ -124,21 +127,52 @@ class Snake:
 
 
     def get_information(self):
-        # directional information: Do one hot encoding based on which direction the snake's head is moving
-        # You can skip one of the directions because it is redundant since the degrees of freedom for direction is 3
+        # Snake's head direction information: Do one hot encoding based on which direction the snake's head is moving
+        # You can skip one of the directions because it is redundant since the degrees of freedom for direction is 3, this would be the approach IF my snake started automatically moving from the start
+        # Which means, you cant skip any of the directions because they are all needed. At the start all the directions are 0 which means the degrees of freedom has to be 4 for direction
         if self.velocity == (0, -Snake.TILE_SIZE):
-            self.information[0] = 1
+            self.information[0] = 1                 # snake is heading up
         elif self.velocity == (0, Snake.TILE_SIZE):
-            self.information[1] = 1
+            self.information[1] = 1                 # snake is heading down
         elif self.velocity == (-Snake.TILE_SIZE, 0):
-            self.information[2] = 1
+            self.information[2] = 1                 # snake is heading left
+        else:
+            self.information[3] = 1                 # snake is heading right
 
-        # TODO: add the remaining 13 informations for the input layer
+        # Food direction information: Do one hot encoding again but for food this time
+        # You cant skip any of the directions for food. The food might be above the snake's head but you also have to know if its to your right or to your left or neither. dof is 4
+        if self.food.y < self.snake[0].y:
+            self.information[4] = 1                 # snake is below food
+        elif self.food.y > self.snake[0].y:
+            self.information[5] = 1                 # snake is above food
+        elif self.food.x < self.snake[0].x:
+            self.information[6] = 1                 # snake is to the right of food
+        elif self.food.x > self.snake[0].x:
+            self.information[7] = 1                 # snake is to the left of food
+
+        # Danger information: Do one hot encoding again but for the immediate danger to the snake's head (e.g border and body part)
+        if ((self.snake[0].y - Snake.TILE_SIZE) == -50 or
+            (self.snake[0].move(0, -Snake.TILE_SIZE)) in self.snake):
+            self.information[8] = 1                 # there is immediate danger above the snake's head
+        elif ((self.snake[0].y + Snake.TILE_SIZE) == Snake.GAME_WIDTH or
+            (self.snake[0].move(0, Snake.TILE_SIZE)) in self.snake):
+            self.information[9] = 1                 # there is immediate danger below the snake's head
+        elif ((self.snake[0].x + Snake.TILE_SIZE) == Snake.GAME_WIDTH or
+            (self.snake[0].move(Snake.TILE_SIZE, 0)) in self.snake):
+            self.information[10] = 1                # there is immediate danger to the right of the snake's head
+        elif ((self.snake[0].x - Snake.TILE_SIZE) == -50 or
+            (self.snake[0].move(-Snake.TILE_SIZE, 0)) in self.snake):
+            self.information[11] = 1                # there is immediate danger to the left of the snake's head
+
+        # Danger information 2: Instead of ohe i will use distance of the border to the snake's head from every direction
+        self.information[12] = self.snake_x - 0     # distance from top border
+
+        # TODO: add the remaining informations for the input layer
         
 
 # make sure that the code is being accessed from the snake.py module
 if __name__ == "__main__":
 
-    snake = Snake()
-    snake.main()
+    snake_0 = Snake()
+    snake_0.main()
     pygame.quit()
