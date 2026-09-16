9bd9e81e359de6a09859c9eb5290a3f3a7787e89
elnurbairamovv <elnur.bairamov@stu.ocsb.ca>
2026-09-16 13:18:21 -0400

    I made a major reform to the code. check desc for info.
    Basically, my snake game worked when a user played it but I had to make it so that a neural network could play it, not a user. I decided to get rid of main method which contained a huge portion of my code and decided to create a way for the neural network to interact with the game by adding the new step method. I also did a bunch of debugging. I'll add a way to visualize later so I can see how my network is actually doing. Once I am finished with the snake game the training should be straightforward, the whole reason of snake is so I can get information for my network and to do that I have to write snake in a way that is acceptable for the network.

--- snake.py
+++ snake.py
@@ -20,71 +20,66 @@ class Snake:
     def __init__(self):
         self.reset()                            # initialize instance variables using reset method
 
-    def main(self) -> None:
-        pygame.init()                               # initialize pygame
-        window = pygame.display.set_mode(           # display the window
-        size=(
-            Snake.GAME_WIDTH,
-            Snake.GAME_HEIGHT)
-        )
-        clock = pygame.time.Clock()
 
-        while self.running:
+    def step(self, action: int) -> list:
+        self.apply_action(action)               # change the snake's velocity given the network's input
 
-            window.fill(Snake.BLACK)
-            self.draw_grid(window=window)
-            
-            for event in pygame.event.get():        # track every event that the user can do
-                if event.type == pygame.QUIT:       # quit the game if the user clicks the X button at the top right
-                    self.running = False
+        self.update_body()                      # update each part in snake's body
+
+        self.food_collision()                   # check if snake collides with food
+
+        self.border_collision()                 # check if snake collides with border
+
+        self.body_collision()                   # check if snake's head collides with snake's body
+
+        self.starvation()                       # check if the snake hasn't eaten in a long time
+
+        return self.get_information()           # get the board state and return it. this information will be the input layer of the network
+
+
+    def apply_action(self, action: int) -> None:
+        if action == 0 and self.velocity != (0, Snake.TILE_SIZE):
+            self.velocity = (0, -Snake.TILE_SIZE)
+        elif action == 1 and self.velocity != (0, -Snake.TILE_SIZE):
+            self.velocity = (0, Snake.TILE_SIZE)
+        elif action == 2 and self.velocity != (-Snake.TILE_SIZE, 0):
+            self.velocity = (Snake.TILE_SIZE, 0)
+        elif action == 3 and self.velocity != (Snake.TILE_SIZE, 0):
+            self.velocity = (-Snake.TILE_SIZE, 0)
+
+
+    def update_body(self) -> None:
+        old_positions = [snake_part.copy() for snake_part in self.snake]
+        self.snake[0].move_ip(self.velocity)
+
+        for i in range(1, len(self.snake)):
+            self.snake[i] = old_positions[i - 1]
+
+
+    def food_collision(self) -> None:
+        if self.snake[0].center == self.food:
+            self.snake.append(self.food)
+            # generate a new food in the board        
+            food_x, food_y = self.get_random_pos()
+            self.food = pygame.Rect(food_x, food_y, Snake.TILE_SIZE, Snake.TILE_SIZE)
+            self.steps_since_eaten = 0
+        else:
+            self.steps_since_eaten += 1
+
+
+    def border_collision(self) -> None:
+        if self.snake[0].x == Snake.GAME_WIDTH or self.snake[0].y == Snake.GAME_HEIGHT or self.snake[0].x == -Snake.TILE_SIZE or self.snake[0].y == -Snake.TILE_SIZE:
+            self.running = False                # if the snake collides with the border then end the game
     
-                if event.type == pygame.KEYDOWN:    # check if the user pressed down a key on their keyboard
-                    if (event.key == pygame.K_UP) and (self.velocity != (0, Snake.TILE_SIZE)) and not (self.turned_this_frame):
-                        self.velocity = (0, -Snake.TILE_SIZE)
-                        self.turned_this_frame = True
-                    elif (event.key == pygame.K_DOWN) and (self.velocity != (0, -Snake.TILE_SIZE)) and not (self.turned_this_frame):
-                        self.velocity = (0, Snake.TILE_SIZE)
-                        self.turned_this_frame = True
-                    elif (event.key == pygame.K_RIGHT) and (self.velocity != (-Snake.TILE_SIZE, 0)) and not (self.turned_this_frame):
-                        self.velocity = (Snake.TILE_SIZE, 0)
-                        self.turned_this_frame = True
-                    elif (event.key == pygame.K_LEFT) and (self.velocity != (Snake.TILE_SIZE, 0)) and not (self.turned_this_frame):
-                        self.velocity = (-Snake.TILE_SIZE, 0)
-                        self.turned_this_frame = True
 
-            self.snake[0].move_ip(self.velocity)                      # move the head of the snake
+    def body_collision(self) -> None:
+        if self.snake[0] in self.snake[1:]:
+            self.running = False                # if the snake's head collides with the snake's body then end the game
+
             
-            if not (window.get_rect().contains(self.snake[0])):       # if the snake is outside of the board then end the game
-                self.running = False
-            
-            if (self.snake[0] in self.snake[1:]):                     # if the snake head collides with the body then end the game
-                self.running = False
-
-            if self.snake[0].center == self.food.center:              # if the snake head collides with food then add to snake length
-                self.snake.append(self.food)
-    
-                if len(self.snake) == (Snake.ROWS * Snake.COLUMNS):   # check if the snake covers the entire board
-                    self.running = False
-                else:                                                 # generate a new food in the board otherwise
-                    food_x, food_y = self.get_random_pos()
-                    self.food = pygame.Rect(food_x, food_y, Snake.TILE_SIZE, Snake.TILE_SIZE)
-
-            for snake_part in self.snake:                             # draw the snake on the board
-                pygame.draw.rect(surface=window, color=Snake.GREEN, rect=snake_part)
-    
-            for i in range(len(self.snake) - 1, 0, -1):               # this loop updates each part in snake list
-                self.snake[i] = self.snake[i - 1].copy()
-
-            if self.turned_this_frame:
-                self.get_information()                                # get all the informations
-            else:
-                self.information = [0 for _ in range(22)]             # reset the informations for next turn
-
-            self.turned_this_frame = False          # reset snake having turned this turn
-
-            pygame.draw.rect(surface=window, color=Snake.RED, rect=self.food)
-            pygame.display.update()                 # refresh game window
-            clock.tick(10)                          # 10 frames per second
+    def starvation(self) -> None:
+        if self.steps_since_eaten >= 100:
+            self.running = False                # end the game if the snake hasn't eaten in a long time
 
 
     def get_random_pos(self) -> tuple[float, float]:
@@ -94,19 +89,6 @@ class Snake:
             random_x, random_y = (randint(0, Snake.COLUMNS - 1) * Snake.TILE_SIZE, randint(0, Snake.ROWS - 1) * Snake.TILE_SIZE)
     
         return random_x, random_y
-
-
-    def simulate_keypress(self, key) -> None:
-        key_event = pygame.event.Event(pygame.KEYDOWN, key=key)
-        
-        pygame.event.post(key_event)
-
-
-    def draw_grid(self, window: pygame.Surface) -> None:
-        for x in range(0, Snake.GAME_WIDTH, Snake.TILE_SIZE):
-            for y in range(0, Snake.GAME_HEIGHT, Snake.TILE_SIZE):
-                rect = pygame.Rect(x, y, Snake.TILE_SIZE, Snake.TILE_SIZE)
-                pygame.draw.rect(surface=window, color=Snake.WHITE, rect=rect, width=1)
 
 
     def reset(self) -> None:
@@ -119,79 +101,98 @@ class Snake:
         food_x, food_y = self.get_random_pos()
         self.food = pygame.Rect(food_x, food_y, Snake.TILE_SIZE, Snake.TILE_SIZE)
 
-        # this variable is used to check if the snake has already moved
-        self.turned_this_frame = False
         # this variable controls whether the game is running or not running
         self.running = True
 
-        # this is the information i will be giving to the input layer of the neural network
-        self.information = [0 for _ in range(22)]
+        # this variable is to check how many steps the snake has taken since having eaten
+        self.steps_since_eaten = 0
 
 
     def get_information(self) -> None:
+        information = [0 for _ in range(22)]
         # Snake's head direction information: Do one hot encoding based on which direction the snake's head is moving
         # You can skip one of the directions because it is redundant since the degrees of freedom for direction is 3, this would be the approach IF my snake started automatically moving from the start
         # Which means, you cant skip any of the directions because they are all needed. At the start all the directions are 0 which means the degrees of freedom has to be 4 for direction
         if self.velocity == (0, -Snake.TILE_SIZE):
-            self.information[0] = 1                 # snake is heading up
+            information[0] = 1                 # snake is heading up
         elif self.velocity == (0, Snake.TILE_SIZE):
-            self.information[1] = 1                 # snake is heading down
+            information[1] = 1                 # snake is heading down
         elif self.velocity == (-Snake.TILE_SIZE, 0):
-            self.information[2] = 1                 # snake is heading left
+            information[2] = 1                 # snake is heading left
         else:
-            self.information[3] = 1                 # snake is heading right
+            information[3] = 1                 # snake is heading right
 
         # Food direction information: Do one hot encoding again but for food this time
         # You cant skip any of the directions for food. The food might be above the snake's head but you also have to know if its to your right or to your left or neither. dof is 4
         if self.food.y < self.snake[0].y:
-            self.information[4] = 1                 # snake is below food
-        elif self.food.y > self.snake[0].y:
-            self.information[5] = 1                 # snake is above food
-        elif self.food.x < self.snake[0].x:
-            self.information[6] = 1                 # snake is to the right of food
-        elif self.food.x > self.snake[0].x:
-            self.information[7] = 1                 # snake is to the left of food
+            information[4] = 1                 # snake is below food
+        if self.food.y > self.snake[0].y:
+            information[5] = 1                 # snake is above food
+        if self.food.x < self.snake[0].x:
+            information[6] = 1                 # snake is to the right of food
+        if self.food.x > self.snake[0].x:
+            information[7] = 1                 # snake is to the left of food
 
         # Danger information: Do one hot encoding again but for the immediate danger to the snake's head (e.g border and body part)
-        if ((self.snake[0].y - Snake.TILE_SIZE) == -50 or
-            (self.snake[0].move(0, -Snake.TILE_SIZE)) in self.snake):
-            self.information[8] = 1                 # there is immediate danger above the snake's head
-        elif ((self.snake[0].y + Snake.TILE_SIZE) == Snake.GAME_WIDTH or
-            (self.snake[0].move(0, Snake.TILE_SIZE)) in self.snake):
-            self.information[9] = 1                 # there is immediate danger below the snake's head
-        elif ((self.snake[0].x + Snake.TILE_SIZE) == Snake.GAME_WIDTH or
-            (self.snake[0].move(Snake.TILE_SIZE, 0)) in self.snake):
-            self.information[10] = 1                # there is immediate danger to the right of the snake's head
-        elif ((self.snake[0].x - Snake.TILE_SIZE) == -50 or
-            (self.snake[0].move(-Snake.TILE_SIZE, 0)) in self.snake):
-            self.information[11] = 1                # there is immediate danger to the left of the snake's head
+        if ((self.snake[0].y == 0) or
+            (self.snake[0].move(0, -Snake.TILE_SIZE)) in self.snake[1:]):
+            information[8] = 1                 # there is immediate danger above the snake's head
 
-        # Danger information 2: Instead of ohe i will use distance of the border to the snake's head from every direction
-        self.information[12] = self.snake[0].y - 0     # distance from top border
-        self.information[13] = self.snake[0].y - 1000  # distance from bottom border
-        self.information[14] = self.snake[0].x - 0     # distance from left border
-        self.information[15] = self.snake[0].x - 1000  # distance from right border
+        if ((self.snake[0].y + Snake.TILE_SIZE) == Snake.GAME_HEIGHT or
+            (self.snake[0].move(0, Snake.TILE_SIZE)) in self.snake[1:]):
+            information[9] = 1                 # there is immediate danger below the snake's head
+
+        if ((self.snake[0].x + Snake.TILE_SIZE) == Snake.GAME_WIDTH or
+            (self.snake[0].move(Snake.TILE_SIZE, 0)) in self.snake[1:]):
+            information[10] = 1                # there is immediate danger to the right of the snake's head
+
+        if ((self.snake[0].x == 0) or
+            (self.snake[0].move(-Snake.TILE_SIZE, 0)) in self.snake[1:]):
+            information[11] = 1                # there is immediate danger to the left of the snake's head
+
+        # Danger information 2: Instead of ohe i will use distance of the border to the snake's head relative to the direction snake is going
+        # straight, left, right
+        if information[0] == 1:                # if snake is heading up then the borders are top border, left border, and right border
+            information[12] = self.snake[0].y - 0
+            information[13] = self.snake[0].x - 0
+            information[14] = 1000 - self.snake[0].x
+
+        elif information[1] == 1:              # if snake is heading down then the borders are down border, right border, and left border
+            information[12] = 1000 - self.snake[0].y
+            information[13] = 1000 - self.snake[0].x
+            information[14] = self.snake[0].x - 0
+
+        elif information[2] == 1:              # if snake is heading left then the borders are left border, bottom border, and top border
+            information[12] = self.snake[0].x - 0
+            information[13] = 1000 - self.snake[0].y
+            information[14] = self.snake[0].y - 0
+
+        elif information[3] == 1:              # if snake is heading right then the borders are right border, top border, and bottom border
+            information[12] = 1000 - self.snake[0].x
+            information[13] = self.snake[0].y - 0
+            information[14] = 1000 - self.snake[0].y
+
 
         # Danger information 3: i will use distance to the closest body part from all directions
         for i in range(0, Snake.GAME_WIDTH, Snake.TILE_SIZE):
-            if (self.snake[0].move(i, 0) in self.snake):
-                self.information[16] = self.snake[0].x - i  # distance to the body part at right
+            if (self.snake[0].move(i, 0) in self.snake[1:]):
+                information[16] = self.snake[0].x - i  # distance to the body part at right
 
         for i in range(0, Snake.GAME_WIDTH, Snake.TILE_SIZE):
-            if (self.snake[0].move(-i, 0) in self.snake):
-                self.information[17] = self.snake[0].x - i  # distance to the body part at left
+            if (self.snake[0].move(-i, 0) in self.snake[1:]):
+                information[17] = self.snake[0].x - i  # distance to the body part at left
             
         for i in range(0, Snake.GAME_HEIGHT, Snake.TILE_SIZE):
-            if (self.snake[0].move(0, i) in self.snake):
-                self.information[18] = self.snake[0].y - i  # distance to the body part at bottom
+            if (self.snake[0].move(0, i) in self.snake[1:]):
+                information[18] = self.snake[0].y - i  # distance to the body part at bottom
 
         for i in range(0, Snake.GAME_HEIGHT, Snake.TILE_SIZE):
-            if (self.snake[0].move(0, -i) in self.snake):
-                self.information[19] = self.snake[0].y - i  # distance to the body part at top
+            if (self.snake[0].move(0, -i) in self.snake[1:]):
+                information[19] = self.snake[0].y - i  # distance to the body part at top
 
         # Food direction information 2: basically the x and y distance from food
-        self.information[20] = self.snake[0].x - self.food.x
-        self.information[21] = self.snake[0].y - self.food.y
+        information[20] = self.snake[0].x - self.food.x
+        information[21] = self.snake[0].y - self.food.y
         
 
 # make sure that the code is being accessed from the snake.py module
